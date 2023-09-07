009c5d36cf477751cb5ee8b0bcda95bfe91e9d5c
Rio Arya <105624375+rioarya01@users.noreply.github.com>
2023-09-07 21:58:59 +0800

    commit chapter 5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="home">
         <h1>All Destinations</h1>
+        <!-- <button @click="triggerRouterError">Trigger Router Error</button> -->
+        <button @click="addDynamicRoute">Add Dynamic Route</button>
+        <router-link to="/dynamic">Visit Dynamic Route</router-link>
         <div class="destinations">
             <router-link v-for="destination in destinations"
             :key="destination.id"
@@ -14,12 +17,34 @@
 </template>
 <script>
 import sourceData from "@/data.json";
+// import { isNavigationFailure, NavigationFailureType } from "vue-router";
 export default {
     name: "Home",
     data() {
         return {
             destinations: sourceData.destinations
         };
+    },
+    methods: {
+        // async triggerRouterError() {
+        //     const navigationFailure = await this.$router.push("/")
+        //     if(isNavigationFailure(navigationFailure, NavigationFailureType.duplicated)){
+        //         console.log(navigationFailure.to)
+        //         console.log(navigationFailure.from)
+        //     } else {
+        //         // all is well
+        //     }
+        // }
+
+        addDynamicRoute() {
+            this.$router.addRoute({
+                name: "dynamic",
+                path: "/dynamic",
+                component: () => import("@/views/Login.vue"),
+            })
+            // Jika fungsi dibawah ini digunakan, maka halaman login pada button add Dynamic Route tidak akan berfungsi dan tidak akan mengarah kehalaman login.
+            this.$router.removeRoute("dynamic")
+        }
     }
 };
 </script>
